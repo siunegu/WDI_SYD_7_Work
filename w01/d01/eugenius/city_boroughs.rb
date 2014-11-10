317a55e043190528ef11d76998769baa146596f2
Eugenius Hei Man Lai <equalsbrackets@gmail.com>
2014-11-10 17:11:50 +1100

    Completed Lab

---
+++ w01/d01/eugenius/city_boroughs.rb
@@ -0,0 +1,1 @@
+puts "Please type in what Region of Sydney you live in!"
---
+++ w01/d01/eugenius/ruby_conditionals.rb
@@ -0,0 +1,7 @@
+puts "What is your age?"
+age = gets.strip.to_i
+if age < 21
+	puts "You Can't Drink Yet!!! Git Out!!!"
+else 
+	puts "What Would You like Today Good Sir???"
+end
