7a66afb2670757aa9a44e1e7168a2e1bf7ef42b5
Jonathan Hooker <jhooker@phonicshero.com>
2014-11-10 15:01:41 +1100

    hola

--- w01/d01/jonathan/string2.rb
+++ w01/d01/jonathan/string2.rb
@@ -3,7 +3,15 @@ puts "Whats your name punk?"
 name = gets.strip
 
 puts "I've just searched your computer and you have some filthy shit on here"
-puts "Your the kind of guy that wanks over his granny...."
+puts "You're the kind of guy that wanks over his granny...."
+puts "Where do you live"
+address = gets.strip
+
+
+puts "I've just searched your computer and you have some filthy shit on here"
+puts "You're the kind of guy that wanks over his granny...."
+
+
 
 
 
