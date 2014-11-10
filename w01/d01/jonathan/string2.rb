9851747a51ec40bddeb57aff80e4a4896c64c87c
Jonathan Hooker <jhooker@phonicshero.com>
2014-11-10 14:57:15 +1100

    add all

---
+++ w01/d01/jonathan/string2.rb
@@ -0,0 +1,9 @@
+puts "Hey you fucking prick! Don't look at me!... go on... hit enter, I dare you!"
+puts "Whats your name punk?"
+name = gets.strip
+
+puts "I've just searched your computer and you have some filthy shit on here"
+puts "Your the kind of guy that wanks over his granny...."
+
+
+
