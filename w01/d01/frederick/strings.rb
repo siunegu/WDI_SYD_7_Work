0559fef0070018d28d005e5515e251af4d1d4136
Frederick Wong <Freddy@Fredericks-MacBook-Air.local>
2014-11-10 15:01:46 +1100

    first day's files

---
+++ w01/d01/frederick/strings.rb
@@ -0,0 +1,17 @@
+puts "what is your first name?"
+
+first = gets.chomp
+puts "Your first name is #{first}"
+
+puts "What is your last name?"
+last = gets.strip
+puts "Your last name is #{last}"
+
+puts "Your full name is #{first} #{last}"
+
+fullname = "#{first} #{last}"
+
+puts "What is your address?"
+
+address = gets.strip
+puts " Your name is #{fullname} and you live at #{address}"
