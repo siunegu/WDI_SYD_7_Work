21fb18c471ec4ba40f284bbb167499495ef6248b
Joel Hampton <joelhampton@gmail.com>
2014-11-10 17:11:01 +1100

    Completed HW

---
+++ w01/d01/joel/city_boroughs.rb
@@ -0,0 +1,18 @@
+puts "Welcome to the annual meeting of the Australian Street Gangs.\n"
+puts "To place you in the appropiate gang we'll \n need to know what 'hood you claim."
+puts "Are you from north, west, east, or south Sydney?"
+gang = gets.strip.downcase
+puts "You're from the #{gang}"
+
+if gang == "north"
+	puts "\nAhh... You're a Norteno`!! You're officially a member of the bloods!"
+
+elsif gang == "west"
+	puts "\nWestsider, huh? Left coast is the best coast! Bloods for life!"
+
+elsif gang == "east"
+	puts "\nEastern Locs in the house!! You're officially a member of the Sharks.. Hit the beach!"
+
+else gang == "south"
+	puts "\nSOUTHSIDE!! El Diablos are the gang for you!! That's whats up!"
+end		
---
+++ w01/d01/joel/drinking_age.rb
@@ -0,0 +1,7 @@
+puts "Howdy there partner.. I'm going to need to see some ID before selling you some liquor. How old are you?"
+age = gets.to_i
+if age <= 20
+	puts "\nYou're a litte too young to buy liquor."
+	else
+	puts "\nThank you!  Enjoy your drunk!"
+end
