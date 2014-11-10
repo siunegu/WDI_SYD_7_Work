d7ab6fe2dece8b8b4a13bf5372bb1b41693586a9
Chris Ma <crispy-studios@hotmail.com>
2014-11-10 15:20:25 +1100

    Homework

---
+++ w01/d01/chris/learning_strings.rb
@@ -0,0 +1,13 @@
+puts "hello world" #Does not return a value
+p "Hello Sailor" #Also returns a value
+puts "Hello World!".inspect
+
+name="Clayton"
+p "My name is " +name
+p 'My name is\' #{name}'
+
+#Gettin input using gets.chomp
+name=gets #Gets input up to return (also gets return)
+p "my name is #{name}"
+
+puts "This is #{name}" 
