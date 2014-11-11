5ea0ff1b1bd2aa5bd3e51faa6a366193ce9e26f4
Joel Hampton <joelhampton@gmail.com>
2014-11-12 09:05:04 +1100

    For pull request

--- w01/d01/joel/city_boroughs.rb
+++ w01/d01/joel/city_boroughs.rb
@@ -3,15 +3,18 @@ puts "To place you in the appropiate gang we'll \n need to know what 'hood you c
 puts "Are you from north, west, east, or south Sydney?"
 gang = gets.strip.downcase
 
-if gang == "north"
+case gang
+when "north"
 	puts "\nAhh... You're a Norteno`!! You're officially a member of the bloods!"
 
-elsif gang == "west"
+when "west"
 	puts "\nWestsider, huh? Left coast is the best coast! Bloods for life!"
 
-elsif gang == "east"
+when gang == "east"
 	puts "\nEastern Locs in the house!! You're officially a member of the Sharks.. Hit the beach!"
 
-else gang == "south"
+when gang == "south"
 	puts "\nSOUTHSIDE!! El Diablos are the gang for you!! That's whats up!"
+else
+	puts "You ain'f from around here.  Kick rocks, chief!"
 end		
---
+++ w01/d01/joel/leap_year.rb
@@ -0,0 +1,9 @@
+#Leap Year
+
+year = gets.to_i
+
+if year % 4 = 0
+  puts "#{year} is a leap year"
+else
+  puts "#{year} is just a normal year"
+end
