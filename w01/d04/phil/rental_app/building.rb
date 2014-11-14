07d9a06626b41245dde531020924c1e450bc08cc
pdhancock <philipdhancock@gmail.com>
2014-11-14 15:07:16 +1100

    Added stuff

--- w01/d04/phil/rental_app/building.rb
+++ w01/d04/phil/rental_app/building.rb
@@ -1,25 +1,30 @@
 
 class Building
-  attr_accessor :address, :style, :has_doorman, :is_walk_up, :num_floors
+  attr_accessor :address, :style, :has_doorman, :is_walk_up, :num_floors, :num_apartments
 
-  def initialize(address, style, has_doorman, is_walkup, num_floors)
+  def initialize(address, style, has_doorman, is_walkup, num_floors, num_apartments)
     self.address = address
     self.style = style
     self.has_doorman = has_doorman
     self.is_walk_up = is_walk_up
     self.num_floors = num_floors
+    self.num_apartments = num_apartments
   end
 
   def to_s
-    puts "Building"
+    string = "Building\n"
+    string += "Address: #{self.address}\n"
+    string += "Style: #{self.style}\n"
+    string += "Has Doorman?: #{self.has_doorman}\n"
+    string += "Is Walk Up?:#{self.is_walk_up}\n"
+    string += "Num floors: #{self.num_floors}\n"
+    string += "Num Apartments: #{self.num_apartments}"
+
   end  
 
 end
 
 
-Tower = Building.new("Sydney", "High Rise", true, false, 10)
-Flats = Building.new("Newtown", "Townhouses", false, true, 3)
-Dorms = Building.new("Randwick","Dorms", false, true, 3)
 
 
 
--- w01/d04/phil/rental_app/test.rb
+++ w01/d04/phil/rental_app/test.rb
@@ -1,6 +1,4 @@
-require_relative "building"
-
-class Apartment < Building
+  class Apartment 
 
   attr_accessor :building, :price, :is_occupied, :sqft, :num_beds, :num_baths
 
@@ -13,30 +11,18 @@ class Apartment < Building
     self.num_baths = num_baths
   end
 # end
-  # def to_s
-  #   puts "This apartment is in the #{self.building} it is #{self.price} per week"
-  #   puts "It is #{self.sqft} square feet, has #{self.num_beds} beds and #{self.num_baths} bathroomes."
-  #   if self.is_occupied == false
-  #     puts "It is currently unoccupied "
-  #   else
-  #     puts "It is currently occupied"
-  #   end
-  # end
-
-  def to_hash
-    self.class = {
-      building: self.building, 
-      price: self.price, 
-      is_occupied: self.is_occupied,
-      sqft: self.sqft,
-      num_beds: self.num_beds,
-      num_baths: self.num_baths
-    }
+  def to_s
+    puts "This apartment is in the #{self.building} it is #{self.price} per week"
+    puts "It is #{self.sqft} square feet, has #{self.num_beds} beds and #{self.num_baths} bathroomes."
+    if self.is_occupied == false
+      puts "It is currently unoccupied "
+    else
+      puts "It is currently occupied"
+    end
   end
-
 end
 
-def create_apartment_list
+
   apartment_list = []
   apartment_list << Apartment.new("Tower", 500, false, 1000, 2, 2)
   apartment_list << Apartment.new("Tower", 600, true, 1300, 2, 2)
@@ -53,19 +39,7 @@ def create_apartment_list
   apartment_list << Apartment.new("Dorms", 700, true, 700, 2, 2)
   apartment_list << Apartment.new("Dorms", 400, false, 600, 1, 1)
   apartment_list << Apartment.new("Dorms", 650, false, 1200, 2, 2)
-  
-  return apartment_list
-end
 
 
-puts apartments = create_apartment_list
-vacant = []
-vacant << apartments.reject {|x| x.is_occupied}
-
-# puts buildings_with_vacant = vacant.select {|x| x.building}
-
- 
-
-
-
+p apartment_list.reject {|x| x.is_occupied}
 
