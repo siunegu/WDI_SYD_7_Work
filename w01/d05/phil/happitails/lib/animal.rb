bdec4e58edf9d0ae539a3b0f481090da28f343cb
pdhancock <philipdhancock@gmail.com>
2014-11-15 17:17:22 +1100

    starting HW

---
+++ w01/d05/phil/happitails/lib/animal.rb
@@ -0,0 +1,23 @@
+
+
+class Animal
+
+  attr_accessor :name, :age, :gender, :species, :toys
+ 
+  def initialize(name, age, gender, species)
+ 
+  end
+ 
+  def to_s
+ 
+  end
+
+  def adopt
+
+  end
+
+  def put_up_for_adoption
+
+  end
+
+end
