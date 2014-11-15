6eb0314a3d03e6b18b95f20d1f40957ae3999f64
pdhancock <philipdhancock@gmail.com>
2014-11-16 00:04:30 +1100

    update happitrails

--- w01/d05/phil/happitails/lib/shelter.rb
+++ w01/d05/phil/happitails/lib/shelter.rb
@@ -2,17 +2,30 @@
 
 class Shelter
 
-  attr_accessor :clients, :animals
- 
-  def initialize(clients, animals)
-  
-  self.clients = clients
-  self.animals = animals
+  attr_accessor :name, :clients, :animals
+
+  def initialize(name)
+    self.name = name
+    clients = []
+    @@animals = Array.new
+
+  end
+
+  def add_animal_to_shelter(name, age, gender, species, toys)
+    new_animal = {:name => name, :age => age, :gender=> gender, :species => species, :toys => toys}
+    @@animals << new_animal
+    puts "The animals at #{name} are: #{@@animals}" # rewrite with an each
+    gets
+    Menu.new
+  end
+
+  def to_s
+    puts "this shelter is #{name}"
+
+  end
+
+
+end
 
  
-  end
- 
-  def to_s
- 
-  end
-end
+  
